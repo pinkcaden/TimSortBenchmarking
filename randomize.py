8612fa250fbe81b393f1a821d555f376f4001f35
Caden Pink <cadenpink@gmail.com>
2025-11-09 17:26:02 -0600

    setup-randomized-factory
    Going to set up factory for creating randomized array objects

---
+++ randomize.py
@@ -0,0 +1,12 @@
+
+
+class RandomizedArray:
+    def __init__(self, array):
+        pass
+
+
+class RandomizedArrayFactory:
+    def __init__(self):
+        pass
+    def get_randomized_array(self, array):
+        return RandomizedArray(array)
